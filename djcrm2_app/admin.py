18068bcb5c3b6ba1c814208bf9081a290780e447
barnez299 <barnez29@gmail.com>
2021-02-25 01:11:56 +0200

    user model updated

--- djcrm2_app/admin.py
+++ djcrm2_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Lead, LeadAssigned, LeadStatus
+
+
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(LeadAssigned)
+admin.site.register(LeadStatus)
--- djcrm2_app/models.py
+++ djcrm2_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20, default=None)
+    last_name = models.CharField(max_length=20, default=None)
+
+class LeadAssigned(models.Model):
+    pass
+
+class LeadStatus(models.Model):
+    pass
